dbf8174f11abfd5c2ec9112223772610d5ed4d0d
Nirmal raj <128925724+nirmalraj002@users.noreply.github.com>
2024-08-03 08:35:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-API_ID = "26482070"
-API_HASH = "28f1d67e2693e057ae4611521c555136"
+API_ID = "29174394"
+API_HASH = "2c83df6681819061fb6951c8d482b661"
 
-BOT_TOKEN = "6557432874:AAGywcrSYYwKE9o_r3foRzfVM6khPLgPMxk"
-OWNER_ID = "6441456023"
+BOT_TOKEN = "7254375706:AAF_ZivIzvn8Kzn2JshsktiRIU-1c5-ay90"
+OWNER_ID = "6916055129"
 
-MONGO_DB_URI = "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URI = "mongodb+srv://vasudurairam:vasudurai123@cluster01.3l3sszt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01"
 MUST_JOIN = "dev_gagan"
